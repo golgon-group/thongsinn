5b86e244d86bacb07108cf3cbc2c6d9ae4641fb0
alvin2310 <56673870+alvin2310@users.noreply.github.com>
2020-10-16 10:55:21 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -155,7 +155,7 @@
                 <!-- Content -->
                 <div class="room-content">
                     <h2 data-animation="fadeInUp" data-delay="100ms">Cottage Room</h2>
-                    <h3 data-animation="fadeInUp" data-delay="100ms">Start from IDR 320.000,- Nett <span>/ Day</span></h3>
+                    <h3 data-animation="fadeInUp" data-delay="100ms">Start from IDR 210.000,- Nett <span>/ Night</span></h3>
                     <ul class="room-feature" data-animation="fadeInUp" data-delay="100ms">
                         <li><span><i class="fa fa-check"></i> Bed</span> <span>: Twin Beds</span></li>
                         <li><span><i class="fa fa-check"></i> Capacity</span> <span>: Max person 2</span></li>
